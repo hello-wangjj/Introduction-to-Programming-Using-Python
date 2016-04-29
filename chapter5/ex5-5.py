3fd253ac1b534852dcba7c715d9655a397963ad7
wangjj_win <576988736@qq.com>
2016-04-29 22:32:22 +0800

    chapter5-1

---
+++ chapter5/ex5-5.py
@@ -0,0 +1,7 @@
+data=eval(input('enter an integer(the input ends'+' if it is 0):'))
+sum=0
+while  data!=0:
+	# pass
+	sum+=data
+	data=eval(input('enter an integer(the input ends'+' if it is 0):'))
+print ('the sum is',sum)
