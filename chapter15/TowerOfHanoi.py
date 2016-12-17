a63914d87a4a71cb9043a69aaf85fa14b9cfa0b9
wangjj <wangjj886688@163.com>
2016-12-17 12:15:08 +0800

    python 进阶强化训练

--- chapter15/TowerOfHanoi.py
+++ chapter15/TowerOfHanoi.py
@@ -1,14 +1,15 @@
 def main():
-	n=eval(input('enter number of disks : '))
-	print('The moves are')
-	move(4,'A','B','C')
+    n = eval(input('enter number of disks : '))
+    print('The moves are')
+    move(n, 'A', 'B', 'C')
 
-def move(n,fromTower,toTower,auxTower):
-	if n==1:
-		print('Move disk ',n,'from',fromTower,'to',toTower)
-	else:
-		move(n-1,fromTower,auxTower,toTower)
-		print('Move disk ',n,'from',fromTower,'to',toTower)
-		move(n-1,auxTower,toTower,fromTower)
-if __name__=='__main__':
-	main()
+
+def move(n, fromTower, toTower, auxTower):
+    if n == 1:
+        print('Move disk ', n, 'from', fromTower, 'to', toTower)
+    else:
+        move(n - 1, fromTower, auxTower, toTower)
+        print('Move disk ', n, 'from', fromTower, 'to', toTower)
+        move(n - 1, auxTower, toTower, fromTower)
+if __name__ == '__main__':
+    main()
---
+++ more_effective/1.1.py
@@ -0,0 +1,25 @@
+#!python3
+# -*- coding: utf-8 -*-
+from random import randint, sample
+from functools import reduce
+__author__ = 'wangjj'
+__mtime__ = '201612171:08'
+'''
+从多个字典中取得公共的键值
+'''
+s = 'abcdefghi'
+
+s1 = {x: randint(3, 6) for x in sample(s, randint(3, 6))}
+s2 = {x: randint(3, 6) for x in sample(s, randint(3, 6))}
+s3 = {x: randint(3, 6) for x in sample(s, randint(3, 6))}
+print(s1,s2,s3,sep='\n----\n')
+# one
+res=[]
+for k in s1.keys():
+    if k in s2.keys() and k in s3.keys():
+        res.append(k)
+
+# the good one
+# map(dict.keys(),[s1,s2,s3])
+# reduce
+print(reduce(lambda a,b:a&b ,map(dict.keys,[s1,s2,s3])))
