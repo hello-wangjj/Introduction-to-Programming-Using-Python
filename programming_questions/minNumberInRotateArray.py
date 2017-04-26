d95744bee538153966b56c62950142266a06ab33
wangjj <wangjj886688@163.com>
2017-04-26 22:29:43 +0800

    '2017.04.26'

---
+++ programming_questions/minNumberInRotateArray.py
@@ -0,0 +1,23 @@
+#!python3
+# -*- coding: utf-8 -*-
+def minNumberInRotateArray(rotateArray):
+        # write code here
+    if len(rotateArray) == 0:
+        return 0
+    target = rotateArray[0]
+    for i in rotateArray[1:]:
+        if target > i :
+            return i
+        target = i
+    return target
+        
+        
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    print(minNumberInRotateArray([3,4,5,1,2]))
+    print(minNumberInRotateArray([1,1,1,0,1,1]))
