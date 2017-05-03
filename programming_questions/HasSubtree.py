7c06b13de84f7aa9b82e54db3c7c13aba5388fff
wangjj <wangjj886688@163.com>
2017-05-03 16:20:05 +0800

    hasSubtree

--- programming_questions/HasSubtree.py
+++ programming_questions/HasSubtree.py
@@ -8,3 +8,15 @@
 class Solution:
     def HasSubtree(self, pRoot1, pRoot2):
         # write code here
+        if not pRoot1 or not pRoot2:
+            return False
+        return self.isSubtree(pRoot1,pRoot2) or self.HasSubtree(pRoot1.left,pRoot2) or self.HasSubtree(pRoot1.right,pRoot2)
+        
+
+    def isSubtree(self,A,B):
+        if not B:
+            # 如果 B为空
+            return True
+        if not A or A.val != B.val:
+            return False
+        return self.isSubtree(A.left,B.left) adn self.isSubtree(A.right,B.right)
