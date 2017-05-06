c636a7c6daeff01a8942dfd642738c21382db3d8
wangjj <wangjj886688@163.com>
2017-05-06 20:42:16 +0800

    定义栈的数据结构，请在该类型中实现一个能够得到栈最小元素的min函数。

---
+++ programming_questions/defineStack.py
@@ -0,0 +1,22 @@
+# 定义栈的数据结构，请在该类型中实现一个能够得到栈最小元素的min函数。
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, node):
+        # write code here
+        self.stack.append(node)
+        if not self.min_stack or node<=self.min_stack[-1]:
+            self.min_stack.append(node)
+    def pop(self):
+        # write code here
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+    def top(self):
+        # write code here
+        return self.stack[-1]
+    def min(self):
+        # write code here
+        return self.min_stack[-1]
