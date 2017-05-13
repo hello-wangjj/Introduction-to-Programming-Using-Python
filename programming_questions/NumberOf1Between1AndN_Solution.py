527731f5e63b9adfeb95a64cca001ea8e91d0885
wangjj <wangjj886688@163.com>
2017-05-13 19:48:14 +0800

    求出任意非负整数区间中1出现的次数。

---
+++ programming_questions/NumberOf1Between1AndN_Solution.py
@@ -0,0 +1,17 @@
+# 求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？
+# 为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+# ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数。
+# class Solution:
+#     def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+def NumberOf1Between1AndN_Solution(n):
+    if n<0:
+        return 0
+    ls = []
+    for i in range(n+1):
+        ls.extend(list(str(i)))
+    return ls.count('1')
+
+
+print(NumberOf1Between1AndN_Solution(13))
+    
