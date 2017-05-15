75c281c1519a64c4a4ba1000d43d5a211747e8fc
wangjj <wangjj886688@163.com>
2017-05-15 15:56:33 +0800

    输入一棵二叉树，判断该二叉树是否是平衡二叉树。

---
+++ programming_questions/IsBalanced_Solution.py
@@ -0,0 +1,22 @@
+# 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def IsBalanced_Solution(self, pRoot):
+        # write code here
+        if not pRoot:
+            return True
+        if abs(self.TreeDepth(pRoot.left)-self.TreeDepth(pRoot.right))>1:
+            return False
+        return self.IsBalanced_Solution(pRoot.left) and self.IsBalanced_Solution(pRoot.right)
+    
+    def TreeDepth(self,pRoot):
+        if not pRoot:
+            return 0
+        nLeft = self.TreeDepth(pRoot.left)
+        nRight = self.TreeDepth(pRoot.right)
+        return max(nLeft,nRight)+1
