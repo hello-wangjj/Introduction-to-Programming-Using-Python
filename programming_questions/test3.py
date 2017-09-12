af187b950c66f88cb5327631f8e9b59c8ae6adb0
WANGJJ\wangj <576988736@qq.com>
2017-09-12 20:14:22 +0800

    前后交换

--- programming_questions/test3.py
+++ programming_questions/test3.py
@@ -1,5 +1,4 @@
 import sys
-import math
 
 __author__ = 'wangj'
 __date__ = '2017/09/10 19:43'
@@ -7,12 +6,23 @@ __date__ = '2017/09/10 19:43'
 
 def main():
     s = sys.stdin.readline().strip()
-    l = len(s)
-
-def repeat(l):
-    pass
-
-
+    l = list(s)
+    new_l = []
+    length = len(s)
+    if length % 2 == 0:
+        for i in range(len(l)):
+            if i % 2 == 0:
+                new_l.append(l[i + 1])
+            else:
+                new_l.append(l[i - 1])
+    else:
+        for i in range(len(l) - 1):
+            if i % 2 == 0:
+                new_l.append(l[i + 1])
+            else:
+                new_l.append(l[i - 1])
+        new_l.append(l[-1])
+    print(new_l)
 
 
 if __name__ == '__main__':
