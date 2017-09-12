cea3f297e487b89a60fa873a9f4632b10187a114
WANGJJ\wangj <576988736@qq.com>
2017-09-12 18:38:09 +0800

    文本分类练习

---
+++ NLP/Sougou/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'wangj'
+__date__ = '2017/09/03 20:59'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ programming_questions/test2.py
@@ -0,0 +1,10 @@
+__author__ = 'wangj'
+__date__ = '2017/09/08 20:19'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
