6c74e7f84f28ee19eb4429776baa962a97844f37
WANGJJ\wangj <576988736@qq.com>
2017-10-08 00:46:38 +0800

    莫烦学tensorflow

--- tensorflow_learn/007_dropout.py
+++ tensorflow_learn/007_dropout.py
@@ -3,55 +3,61 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 
-__author__ = 'wangj'
-__date__ = '2017/10/05 11:33'
+# load data
+digits = load_digits()
+X = digits.data
+y = digits.target
+y = LabelBinarizer().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
 
 
-def add_layer(inputs, in_size, out_size, layer_name, activation_func=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size], dtype=tf.float64), dtype=tf.float64)
-    biases = tf.Variable(tf.zeros([1, out_size], dtype=tf.float64) + 0.1, dtype=tf.float64)
+def add_layer(inputs, in_size, out_size, layer_name, activation_function=None, ):
+    # add one more layer and return the output of this layer
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, )
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    if activation_func is None:
-        out_puts = Wx_plus_b
+    # here to dropout
+    Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
+    if activation_function is None:
+        outputs = Wx_plus_b
     else:
-        out_puts = activation_func(Wx_plus_b)
-    tf.summary.histogram(layer_name + '/out_puts', out_puts)
-    return out_puts
+        outputs = activation_function(Wx_plus_b, )
+    tf.summary.histogram(layer_name + '/outputs', outputs)
+    return outputs
 
 
-def main():
-    digits = load_digits()
-    X = digits.data
-    y = digits.target
-    y = LabelBinarizer().fit_transform(y)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    xs = tf.placeholder(dtype=tf.float64, shape=[None, 64])
-    ys = tf.placeholder(dtype=tf.float64, shape=[None, 10])
+# define placeholder for inputs to network
+keep_prob = tf.placeholder(tf.float32)
+xs = tf.placeholder(tf.float32, [None, 64])  # 8x8
+ys = tf.placeholder(tf.float32, [None, 10])
 
-    # add layer
-    l1 = add_layer(X_train, 64, 100, 'l1', activation_func=tf.nn.tanh)
-    prediction = add_layer(l1, 100, 10, 'l2', activation_func=tf.nn.softmax)
+# add output layer
+l1 = add_layer(xs, 64, 50, 'l1', activation_function=tf.nn.tanh)
+prediction = add_layer(l1, 50, 10, 'l2', activation_function=tf.nn.softmax)
 
-    # loss
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_train * tf.log(prediction), reduction_indices=[1]))
-    tf.summary.scalar('loss', cross_entropy)
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-    init = tf.global_variables_initializer()
+# the loss between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))  # loss
+tf.summary.scalar('loss', cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-    with tf.Session() as sess:
-        merged = tf.summary.merge_all()
-        # summary writer goes in here
-        train_writer = tf.summary.FileWriter('./logs/train', sess.graph)
-        test_writer = tf.summary.FileWriter('./logs/test', sess.graph)
-        sess.run(init)
-        for i in range(501):
-            sess.run(train_step, feed_dict={xs: X_train, ys: y_train})
-            if i % 50 == 0:
-                train_result = sess.run(merged, feed_dict={xs: X_train, ys: y_train})
-                test_result = sess.run(merged, feed_dict={xs: X_test, ys: y_test})
-                train_writer.add_summary(train_result, i)
-                test_writer.add_summary(test_result, i)
+sess = tf.Session()
+merged = tf.summary.merge_all()
+# summary writer goes in here
+train_writer = tf.summary.FileWriter("logs/train", sess.graph)
+test_writer = tf.summary.FileWriter("logs/test", sess.graph)
 
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
 
-if __name__ == '__main__':
-    main()
+init = tf.global_variables_initializer()
+sess.run(init)
+for i in range(500):
+    # here to determine the keeping probability
+    sess.run(train_step, feed_dict={xs: X_train, ys: y_train, keep_prob: 0.5})
+    if i % 50 == 0:
+        # record loss
+        train_result = sess.run(merged, feed_dict={xs: X_train, ys: y_train, keep_prob: 1})
+        test_result = sess.run(merged, feed_dict={xs: X_test, ys: y_test, keep_prob: 1})
+        train_writer.add_summary(train_result, i)
+        test_writer.add_summary(test_result, i)
--- tensorflow_learn/008_nn_loss.py
+++ tensorflow_learn/008_nn_loss.py
@@ -1,9 +1,41 @@
+import tensorflow as tf
+from numpy.random import RandomState
+
 __author__ = 'wangj'
 __date__ = '2017/10/06 16:31'
 
 
 def main():
-    pass
+    batch_size = 8
+    x = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='x-input')
+    y = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='y-input')
+
+    w1 = tf.Variable(tf.random_normal([2, 1], stddev=1, seed=1))
+    y_hat = tf.matmul(x, w1)
+
+    # loss
+    loss_less = 10
+    loss_more = 1
+    loss = tf.reduce_sum(tf.where(tf.greater(y, y_hat), (y - y_hat) * loss_more, (y_hat - y) * loss_less))
+
+    train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
+
+    rdm = RandomState(1)
+
+    dataset_size = 128
+    X = rdm.rand(dataset_size, 2)
+    Y = [[x1 + x2 + rdm.rand() / 10 - 0.05] for (x1, x2) in X]
+
+    with tf.Session() as sess:
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+        Steps = 5001
+        for i in range(Steps):
+            start = (i * batch_size) % dataset_size
+            end = min(start + batch_size, dataset_size)
+            sess.run(train_step, feed_dict={x: X[start:end], y: Y[start:end]})
+            if i % 100 == 0:
+                print(sess.run(w1))
 
 
 if __name__ == '__main__':
