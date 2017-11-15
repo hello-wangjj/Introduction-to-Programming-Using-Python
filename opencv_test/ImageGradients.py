5ac62cbde6f319d8b3452a3256ed8a7941cfb65c
WANGJJ\wangj <576988736@qq.com>
2017-11-15 20:53:01 +0800

    边缘检测

---
+++ opencv_test/ImageGradients.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+__author__ = 'wangj'
+__date__ = '2017/11/15 15:34'
+
+
+def main():
+    img = cv2.imread('2.jpg', 0)
+
+    laplacian = cv2.Laplacian(img, cv2.CV_64F)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
+
+    plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 2), plt.imshow(laplacian, cmap='gray')
+    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
+    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 4), plt.imshow(sobely, cmap='gray')
+    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ opencv_test/canny_exercise.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+__author__ = 'wangj'
+__date__ = '2017/11/15 20:01'
+
+lowThreshold = 0
+max_lowThreshold = 100
+ratio = 3
+kernel_size = 3
+
+
+def CannyThreshold(lowThreshold):
+    detected_edgs = cv2.GaussianBlur(img_gray, (3, 3), 0)
+    detected_edgs = cv2.Canny(detected_edgs, lowThreshold, lowThreshold * ratio, apertureSize=kernel_size)
+    dst = cv2.bitwise_and(img, img, mask=detected_edgs)
+    cv2.imshow('canny', dst)
+
+
+if __name__ == '__main__':
+    img = cv2.imread('1.jpg')
+    if not img.data:
+        print('false')
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.namedWindow('canny')
+    cv2.createTrackbar('canny 参数：', 'canny', lowThreshold, max_lowThreshold, CannyThreshold)
+    CannyThreshold(0)  # initialization
+    if cv2.waitKey(0) == ord('q'):
+        cv2.destroyAllWindows()
